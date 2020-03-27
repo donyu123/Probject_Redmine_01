411ed737733b2d4c1fa75ba22da7624e4f04fa4f
957859795@qq.com <dyqwaszx132>
2020-03-27 10:46:01 +0800

    自动化框架

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     # 日志报告
     report_path_html = "D:\\Probject_Redmine_01\\report\\result\\html"
     # 执行请求 并且生成报告json文件
-    pytest.main(["-s", "--alluredir", report_path])
+    pytest.main(["-s", "--alluredir", report_path])  
