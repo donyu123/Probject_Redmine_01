2c5dc0971d2414cd06ea6e57f3efb6cf6418920d
957859795@qq.com <dyqwaszx132>
2020-03-27 14:59:07 +0800

    修改启动路径

--- common/redmine_login.py
+++ common/redmine_login.py
@@ -17,7 +17,7 @@ class  RedmineLogin:
     @staticmethod
     def  log_redmine():
         #实例化驱动器"C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe"
-        driver = webdriver.Chrome("C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe")
+        driver = webdriver.Chrome()
         driver.implicitly_wait(10)
         driver.get(url)
         #传入driver 驱动
